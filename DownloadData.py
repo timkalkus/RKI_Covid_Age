390affac748bbfbadd7bd6c89cb67f1b230d5ade
timkalkus <timkalkus@gmail.com>
2021-01-25 12:26:34 +0100

    Clean up

--- DownloadData.py
+++ DownloadData.py
@@ -1,12 +1,6 @@
 from zeep import Client
-import datetime
-import csv
+import datetime, csv, re
 import numpy as np
-import re
-client = Client("https://tools.rki.de/SurvStat/SurvStatWebService.svc?wsdl")
-
-factory = client.type_factory("ns2")
-
 
 
 def formatAgeGroup(AgeGroup):
@@ -19,14 +13,15 @@ def formatAgeGroup(AgeGroup):
         else:
             return start + '-' + stop
 
-    if AgeGroup.find('+') != -1: # == 'A80+':
+    if AgeGroup.find('+') != -1:  # == 'A80+':
         return AgeGroup[1:]
     return AgeGroup
 
 
-
-def clean_fetch(remove_unknown = True):
+def clean_fetch(remove_unknown=True):
     def fetch():
+        client = Client("https://tools.rki.de/SurvStat/SurvStatWebService.svc?wsdl")
+        factory = client.type_factory("ns2")
         res = client.service.GetOlapData(
             {
                 "Language": "German",
@@ -60,7 +55,7 @@ def clean_fetch(remove_unknown = True):
                 # AgeGroupName7 : ['Gesamt' '00-01' '02-04' '05-19' '20-39' '40-59' '60-79' '80+']
                 # AgeGroupName8 : 0,1,2,3,...,78,79,80+
                 # AgeGroupName9 : ['Gesamt' '00-01' '02-09' '10-19' '20-39' '40-59' '60-79' '80+']
-                #10 : ['Gesamt' '00-14' '15-39' '40+']
+                # AgeGroupName10: ['Gesamt' '00-14' '15-39' '40+']
             }
         )
         AgeGroups = [i["Caption"] for i in res.Columns.QueryResultColumn]
@@ -70,32 +65,30 @@ def clean_fetch(remove_unknown = True):
 
     AgeGroups, YearWeek, Data = fetch()
     Data = Data[1:]  # Remove Total over time
-    # Data = Data[1:] # Remove Total over Age Groups
-    # AgeGroups = AgeGroups[1:]
     YearWeek = np.array(YearWeek[1:])
-
     new_Age = np.array(formatAgeGroup(AgeGroups))
-    #YearWeek.insert(0, 'Altergruppe')
-    new_Data = list(zip(*Data[::1]))
-    new_Data = np.array([list(i) for i in new_Data])
+    new_Data = np.array([list(i) for i in list(zip(*Data[::1]))])
     if remove_unknown:
         new_Data = new_Data[new_Age != 'Unbekannt']
         new_Age = new_Age[new_Age != 'Unbekannt']
-    new_Data[new_Data==None]=0
+    new_Data[new_Data == None] = 0
     new_Data = new_Data.astype(np.int)
     return new_Age, YearWeek, new_Data
+
 
 def incidence():
     AgeGroups, YearWeek, Data = clean_fetch()
     age_num = count_age(AgeGroups)
     for i in range(len(age_num)):
-        Data[i] = Data[i]/age_num[i]*100000
+        Data[i] = Data[i] / age_num[i] * 100000
     return AgeGroups, YearWeek, Data
+
 
 def count_age(age_bracket):
     if isinstance(age_bracket, (list, np.ndarray)):
         return [count_age(age) for age in age_bracket]
-    def get_count(age,plus=False):
+
+    def get_count(age, plus=False):
         age_count = [774870, 794132, 802415, 807816, 782143, 771479, 743954, 741969, 725807, 743761, 732376, 751663,
                      746670, 731698, 740506, 756823, 757882, 771938, 798514, 854086, 878869, 905902, 947646, 944591,
                      931423, 947032, 978142, 995515, 1030314, 1123468, 1109227, 1133582, 1109010, 1088700, 1055201,
@@ -109,18 +102,20 @@ def count_age(age_bracket):
         if plus:
             return np.sum(age_count[age:-1])
         return age_count[age]
+
     if age_bracket == 'Unbekannt':
         return -1
-    age_list = re.split(r'[\+\-]',age_bracket)
+    age_list = re.split(r'[+\-]', age_bracket)
     age_list = [age for age in age_list if age != '']
-    if age_list[0]=='Gesamt':
+    if age_list[0] == 'Gesamt':
         return get_count(-1)
-    if len(age_list)==1:
-        if age_bracket.find('+')!=-1:
+    if len(age_list) == 1:
+        if age_bracket.find('+') != -1:
             return get_count(int(age_list[0]), True)
         return get_count(int(age_list[0]))
     else:
-        return np.sum([get_count(i) for i in range(int(age_list[0]),int(age_list[1])+1)])
+        return np.sum([get_count(i) for i in range(int(age_list[0]), int(age_list[1]) + 1)])
+
 
 def yw2datetime(yw):
     if isinstance(yw, (list, np.ndarray)):
@@ -136,35 +131,30 @@ def yw2datetime(yw):
     else:
         return datetime.datetime(yw_int[0] - 1, 12, 31 + date_diff) + datetime.timedelta(weeks=yw_int[1] - 1, days=6)
 
+
 def extrapolateLastWeek(YearWeek, Data):
-    week_diff = (yw2datetime(YearWeek[-1])+datetime.timedelta(hours=24)-datetime.datetime.now())/datetime.timedelta(weeks=1)
+    week_diff = (yw2datetime(YearWeek[-1]) + datetime.timedelta(
+        hours=24) - datetime.datetime.now()) / datetime.timedelta(weeks=1)
 
     if 0 < week_diff < 1:
-        def extrap_function(value,w_diff):
+        def extrap_function(value, w_diff):
             distr = [0, 0.1040, 0.2656, 0.4730, 0.6555, 0.8019, 0.9283, 1.0000]
             # the distribution is based on the reportet cases per weekday in germany from 22.01.2020 to 23.01.2021
-            return value/np.interp(1-w_diff,np.linspace(0,1,len(distr)),distr)
+            return value / np.interp(1 - w_diff, np.linspace(0, 1, len(distr)), distr)
+
         for i in range(len(Data)):
-            Data[i][-1] = extrap_function(Data[i][-1],week_diff)
+            Data[i][-1] = extrap_function(Data[i][-1], week_diff)
         return Data, True
     return Data, False
 
 
-
 def saveCSV():
     new_Age, YearWeek, new_Data = clean_fetch()
-    YearWeek.insert(0,'Altergruppe')
-    #print(new_Data)
+    YearWeek.insert(0, 'Altergruppe')
     for i in range(len(new_Data)):
         new_Data[i].insert(0, new_Age[i])
-    #print(new_Data)
-    #print(list(zip(Data)))
-    with open('currentData.csv', 'wt', newline ='') as file:
+    with open('currentData.csv', 'wt', newline='') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(i for i in YearWeek)
-        #writer.writerow(i for i in YearWeek)
-
         for j in new_Data:
             writer.writerow(j)
-
-#saveCSV()
--- TestFolder/BokehTest.py
+++ TestFolder/BokehTest.py
@@ -1,13 +1,9 @@
-from bokeh.models import HoverTool, Legend
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
-import bokeh.plotting
+from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.plotting import figure, output_file, show
+from bokeh.models.markers import marker_types
 import bokeh.layouts
-
 import datetime
 import numpy as np
-import bokeh.models
-from bokeh.models import ColumnDataSource, Grid, Line, LinearAxis, Plot
-from bokeh.models.markers import marker_types
 
 import DownloadData
 
@@ -18,17 +14,18 @@ np_data[np_data == None] = 0
 
 data = np_data
 
-np.savetxt('age.txt',age,fmt="%s")
-np.savetxt('year_week.txt',year_week,fmt="%s")
-np.savetxt('data.txt',np_data,fmt="%s")
-
+np.savetxt('age.txt', age, fmt="%s")
+np.savetxt('year_week.txt', year_week, fmt="%s")
+np.savetxt('data.txt', np_data, fmt="%s")
 
 age = np.genfromtxt('age.txt', dtype='str')
 year_week = np.genfromtxt('year_week.txt', dtype='str')
 data = np.loadtxt('data.txt')
 
-data, interp = DownloadData.extrapolateLastWeek(year_week,data)
-print('Extrapolated last DataPoint: ',interp)
+data, interp = DownloadData.extrapolateLastWeek(year_week, data)
+print('Extrapolated last DataPoint: ', interp)
+
+
 def yw2datetime(yw):
     if isinstance(yw, (list, np.ndarray)):
         return [yw2datetime(i) for i in yw]
@@ -42,8 +39,6 @@ def yw2datetime(yw):
         return datetime.datetime(yw_int[0], 1, date_diff) + datetime.timedelta(weeks=yw_int[1] - 1, days=6)
     else:
         return datetime.datetime(yw_int[0] - 1, 12, 31 + date_diff) + datetime.timedelta(weeks=yw_int[1] - 1, days=6)
-
-
 
 
 # output to static HTML file
@@ -65,26 +60,16 @@ hover_tool = HoverTool(
 p1 = figure(title="Inzidenz nach Altersgruppen", x_axis_type='datetime', x_axis_label='Datum', y_axis_label='Inzidenz',
             tools='pan,wheel_zoom,box_zoom,reset')
 p1.sizing_mode = "stretch_both"
-label = bokeh.models.Label(x=3,y=3,x_units='screen',y_units='screen',
-                           text='Stand: '+datetime.datetime.now().strftime("%d.%m.%Y %H:%M") +
+label = bokeh.models.Label(x=3, y=3, x_units='screen', y_units='screen',
+                           text='Stand: ' + datetime.datetime.now().strftime("%d.%m.%Y %H:%M") +
                                 '; Quellen: Fallzahlen - Robert Koch-Institut: SurvStat@RKI 2.0, https://survstat.rki.de;' +
                                 ' Bevölkerung: https://www-genesis.destatis.de/ 12411-0005 31.12.2019',
                            text_font_size='8pt')
 #
 p1.add_layout(label)
-p1.xaxis[0].formatter = bokeh.models.DatetimeTickFormatter() # PrintfTickFormatter(format="%d.%m.%Y")
-# hover = p1.select(dict(type=HoverTool))
+p1.xaxis[0].formatter = bokeh.models.DatetimeTickFormatter()  # PrintfTickFormatter(format="%d.%m.%Y")
 
-
-# hover.tooltips = [
-#    ("Alter", "@desc"),
-#    ("Datum", "@x"),
-#    ("Inzidenz", "@y"),
-# ]
-# hover.mode = 'vline'
-# hover.formatters = {'@x':'datetime',}
 cmap_colors = np.zeros([3, len(age)])
-
 
 cmap_colors[0] = np.interp(np.linspace(0, 1, len(age)), np.linspace(0, 1, 4),
                            np.array([17.6, 19.2, 83.1, 83.1]) / 100 * 255)
@@ -96,13 +81,19 @@ cmap_colors[2] = np.interp(np.linspace(0, 1, len(age)), np.linspace(0, 1, 4),
 cmap_colors = cmap_colors.astype(np.uint8)
 
 marker_list = [m_type for m_type in marker_types]
-marker_selector = [0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25]
+marker_selector = [0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15,
+                   16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25,
+                   0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15,
+                   16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25,
+                   0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15,
+                   16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25, 0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25,
+                   0, 1, 6, 8, 14, 15, 16, 12, 21, 24, 25]
 
 glyph_list = []
 
 for i in range(len(age)):
-    if age[i]=='Gesamt':
-        line_color = (0,0,0)
+    if age[i] == 'Gesamt':
+        line_color = (0, 0, 0)
         line_width = 2
     else:
         line_color = tuple(cmap_colors[:, i])
@@ -115,26 +106,23 @@ for i in range(len(age)):
         col=[line_color for x in year_week]
     ))
     if interp:
-        li = p1.line(source.data['x_list'][:-1], source.data['y_list'][:-1], line_color=line_color, line_width=line_width,
-                     line_alpha=1, muted_alpha=muted_alpha,legend_label=age[i])
+        li = p1.line(source.data['x_list'][:-1], source.data['y_list'][:-1], line_color=line_color,
+                     line_width=line_width,
+                     line_alpha=1, muted_alpha=muted_alpha, legend_label=age[i])
         li2 = p1.line(source.data['x_list'][-2:], source.data['y_list'][-2:], line_color=line_color,
                       line_width=line_width,
                       line_alpha=1, muted_alpha=muted_alpha, legend_label=age[i], line_dash=[3, 3])
     else:
         li = p1.line(source.data['x_list'], source.data['y_list'], line_color=line_color, line_width=line_width,
                      line_alpha=1, muted_alpha=muted_alpha, legend_label=age[i])
-    #gly = bokeh.models.X(x="x", y="y", size=10, line_color=line_color, line_width=2, fill_color=None)
-    #gly_p1 = p1.add_glyph(source, gly)
-    sca = p1.scatter(x="x_list", y="y_list", source=source,muted_alpha=muted_alpha,legend_label=age[i])
-    #sca.glyph.marker=marker_list[i]
-    #legend_list.append((age[i], [li]))
+    sca = p1.scatter(x="x_list", y="y_list", source=source, muted_alpha=muted_alpha, legend_label=age[i])
     sca.glyph.marker = marker_list[marker_selector[i]]
     sca.glyph.line_color = line_color
     sca.glyph.fill_color = None
     sca.glyph.size = 8
     glyph_list.append(sca)
 p1.add_tools(HoverTool(
-    renderers = glyph_list,
+    renderers=glyph_list,
     tooltips=[
         ("Alter", "@desc"),
         ("Datum", "@x_list{%d.%m.%Y}"),
@@ -143,20 +131,11 @@ p1.add_tools(HoverTool(
     formatters={'@x_list': 'datetime', },
 ))
 
-#p1.legend(location="top_left",)
-#p1.legend
-#leg = Legend(items=legend_list, location=(0, 0))
 p1.legend.location = "top_left"
 p1.legend.click_policy = "mute"
 p1.legend.orientation = "horizontal"
-#p1.add_layout(leg, 'above')
-p2 = figure(title="simple line example", x_axis_label='x', y_axis_label='y', x_range=p1.x_range)
-p2.line(yw2datetime(year_week), data[i][:], legend_label='None')
-p2.sizing_mode = "stretch_both"
-# p1.multi_line(yw2datetime(year_week[:]),np.transpose(data, (2, 3, 0, 1)))
-column = bokeh.layouts.column([p1])#, p2])
+column = bokeh.layouts.column([p1])  # , p2])
 column.sizing_mode = "stretch_both"
 
-# show the results
 
 show(column)
